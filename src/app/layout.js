a53ea9e8812ef200d64cf3a64c70fac97e6d32e1
RaaziyaMZHF <raziamzh@gmail.com>
2025-02-07 20:02:00 +0530

    Updated service page

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,8 @@ import './globals.css'; // Import your global styles
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
+
+
 export default function Layout({ children }) {
   return (
     <html lang="en">
